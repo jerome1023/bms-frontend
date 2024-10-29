103c96676f208f8927fe1878c6c5274c9922c162
jerome <baerajerome@gmail.com>
2024-10-29 23:28:21 +0800

    create dashboard card

---
+++ components/DashCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-base-gray-300 p-5 rounded-md">
+    <NuxtLink
+      v-if="content"
+      :to="content.href"
+      class="flex gap-5 justify-between cursor-pointer"
+    >
+      <div>
+        <p class="font-bold text-2xl">{{ content.value }}</p>
+        <p class="text-base-gray text-sm">
+          {{ content.value > 1 ? content.label + "s" : content.label }}
+        </p>
+      </div>
+      <FontAwesomeIcon :icon="content.icon" size="lg" class="text-base-green" />
+    </NuxtLink>
+    <div v-else>
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+
+defineProps<{
+  content?: any;
+}>();
+</script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,15 +1,63 @@
 <template>
-    <!-- <header class="bg-base-green">asdas
-        <nav class="bg-base-gray">sdada</nav>
-    </header>
-
-    <aside class="bg-base-gray-light">
-        <details>
-            <summary>side</summary>
-                <p>tesasgjsgsd</p>
-        
-    </details>
-</aside>
-<main class="bg-base-gray">main</main> -->
-retetetet
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
+  >
+    <DashCard v-for="item in list" :content="item" />
+    <DashCard>
+      <div class="flex gap-5 justify-between">
+        <div>
+          <p class="font-bold text-2xl">{{ 0 }} (0)</p>
+          <FormSelect label="Name" name="document" :options="document"/>
+        </div>
+        <FontAwesomeIcon :icon="faCoins" size="lg" class="text-base-green" />
+      </div>
+    </DashCard>
+  </div>
 </template>
+<script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faUsers,
+  faFileLines,
+  faFileCircleCheck,
+  faCircleExclamation,
+  faCoins,
+} from "@fortawesome/free-solid-svg-icons";
+
+const list = ref() as any;
+const document = ref();
+const selectedDocument = ref();
+
+onMounted(async () => {
+  useGetData("dashboard/list").then((response) => {
+    list.value = [
+      {
+        label: "Resident",
+        value: response.resident,
+        icon: faUsers,
+        href: "resident/list",
+      },
+      {
+        label: "Pending Request",
+        value: response.pending,
+        icon: faFileLines,
+        href: "request/pending",
+      },
+      {
+        label: "Approved Request",
+        value: response.approved,
+        icon: faFileCircleCheck,
+        href: "request/approved",
+      },
+      {
+        label: "Blotter",
+        value: response.blotter,
+        icon: faCircleExclamation,
+        href: "blotter",
+      },
+    ];
+    console.log(response.documents)
+    document.value = response.documents;
+  });
+});
+</script>
